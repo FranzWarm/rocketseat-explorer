4c3090977156d2107d6edd7892fc2e59c8d24992
Francisco Warmling <80177177+FranzWarm@users.noreply.github.com>
2023-03-07 16:11:16 -0300

    Update main.jsx

--- nivel09/frontend-rocketmovies/src/main.jsx
+++ nivel09/frontend-rocketmovies/src/main.jsx
@@ -4,13 +4,16 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
 
+import { AuthProvider } from "./hooks/auth";
 import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
